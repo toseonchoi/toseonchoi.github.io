f6c16ff28f39748a5f4587055cda552b39711ce7
toseonchoi <toseonchoi@gmail.com>
2018-04-27 03:55:53 -0700

    update

---
+++ seonchoi_portfolio/js/main.js
@@ -0,0 +1,64 @@
+/*--HEADER-------------------------------------------------------------------------------------------------------*/
+
+
+
+
+/*--GALLERY-------------------------------------------------------------------------------------------------------*/
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("gallery_image");
+  var dots = document.getElementsByClassName("pagination_dot");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" pagination_dot_selected", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " pagination_dot_selected";
+}
+
+/*--JUMP SCROLL-------------------------------------------------------------------------------------------------------*/
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+
+/*--TEST-------------------------------------------------------------------------------------------------------*/
