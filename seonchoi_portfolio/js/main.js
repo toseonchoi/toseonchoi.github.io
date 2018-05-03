60b6498e3214105691fd09f3f9d9b46dd7c42e1d
toseonchoi <toseonchoi@gmail.com>
2018-05-03 04:39:09 -0700

    050318

--- seonchoi_portfolio/js/main.js
+++ seonchoi_portfolio/js/main.js
@@ -1,37 +1,4 @@
-/*--HEADER-------------------------------------------------------------------------------------------------------*/
 
-
-
-
-/*--GALLERY-------------------------------------------------------------------------------------------------------*/
-
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("gallery_image");
-  var dots = document.getElementsByClassName("pagination_dot");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" pagination_dot_selected", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " pagination_dot_selected";
-}
 
 /*--SMOOTH SCROLL-------------------------------------------------------------------------------------------------------*/
 
@@ -59,6 +26,96 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+
+/*--HEADER UP & DOWN-------------------------------------------------------------------------------------------------------*/
+
+
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('header_down').addClass('header_up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('header_up').addClass('header_down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+
+/*--HAMBURGER MENU-------------------------------------------------------------------------------------------------------*/
+
+ 
+    var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+
+    var hamburgers = document.querySelectorAll(".hamburger");
+    if (hamburgers.length > 0) {
+      forEach(hamburgers, function(hamburger) {
+        hamburger.addEventListener("click", function() {
+          this.classList.toggle("is-active");
+        }, false);
+      });
+    }
+
+
+/*--GALLERY-------------------------------------------------------------------------------------------------------*/
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("gallery_image");
+  var dots = document.getElementsByClassName("slide_dot");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" slide_dot_selected", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " slide_dot_selected";
+}
+
+
 
 
 /*--TEST-------------------------------------------------------------------------------------------------------*/
