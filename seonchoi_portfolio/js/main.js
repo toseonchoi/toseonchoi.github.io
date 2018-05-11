9c730d3bb0fabb9a2b8d2370604f486dfe419320
toseonchoi <toseonchoi@gmail.com>
2018-05-11 01:02:54 -0700

    051118 smooth scrolling fix

--- seonchoi_portfolio/js/main.js
+++ seonchoi_portfolio/js/main.js
@@ -1,29 +1,30 @@
 /*--SMOOTH SCROLL-------------------------------------------------------------------------------------------------------*/
 
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+// $(document).ready(function(){
+//   // Add smooth scrolling to all links
+//   $("a").on('click', function(event) {
 
-      // Store hash
-      var hash = this.hash;
+//     // Make sure this.hash has a value before overriding default behavior
+//     if (this.hash !== "") {
+//       // Prevent default anchor click behavior
+//       event.preventDefault();
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 100, function(){
+//       // Store hash
+//       var hash = this.hash;
+
+//       // Using jQuery's animate() method to add smooth page scroll
+//       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//       $('html, body').animate({
+//         scrollTop: $(hash).offset().top
+//       }, 500, function(){
    
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
+//         // Add hash (#) to URL when done scrolling (default click behavior)
+//         window.location.hash = hash;
+//       });
+//     } // End if
+//   });
+// });
 
 
 /*--HEADER UP & DOWN-------------------------------------------------------------------------------------------------------*/
@@ -68,19 +69,32 @@ function hasScrolled() {
 }
 
 
+/*--RETURN TOP------------------------------------------------------------------------------------------------------*/
+
+
+$(window).scroll(function(event){
+    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+        $(".return_top").addClass("show");
+    } else {
+        $(".return_top").removeClass("show");
+    }
+});
+
+
 /*--HAMBURGER MENU-------------------------------------------------------------------------------------------------------*/
 
- 
-    var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
 
-    var hamburgers = document.querySelectorAll(".hamburger");
-    if (hamburgers.length > 0) {
-      forEach(hamburgers, function(hamburger) {
-        hamburger.addEventListener("click", function() {
-          this.classList.toggle("is-active");
-        }, false);
-      });
-    }
+var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+
+var hamburgers = document.querySelectorAll(".hamburger");
+if (hamburgers.length > 0) {
+  forEach(hamburgers, function(hamburger) {
+    hamburger.addEventListener("click", function() {
+      this.classList.toggle("is-active");
+    }, false);
+  });
+}
 
 
 /*--GALLERY-------------------------------------------------------------------------------------------------------*/
@@ -114,28 +128,68 @@ function showDivs(n) {
 }
 
 
-/*--RETURN TOP------------------------------------------------------------------------------------------------------*/
-
-
-$(window).scroll(function(event){
-    var scroll = $(window).scrollTop();
-    if (scroll >= 50) {
-        $(".return_top").addClass("show");
-    } else {
-        $(".return_top").removeClass("show");
-    }
-});
-/*Animation anchor*/
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 1000);
-});
-
-
-/*--TEST-------------------------------------------------------------------------------------------------------*/
+/*--MOBILE HOVER-------------------------------------------------------------------------------------------------------*/
 
 
 +$('body').bind('touchstart', function() {});
 
 +document.addEventListener("touchstart", function(){}, true); 
+
+
+/*--TEST-------------------------------------------------------------------------------------------------------*/
+
+
+(function() // Code in a function to create an isolate scope
+{
+var speed = 500;
+var moving_frequency = 15; // Affects performance !
+var links = document.getElementsByTagName('a');
+var href;
+for(var i=0; i<links.length; i++)
+{   
+    href = (links[i].attributes.href === undefined) ? null : links[i].attributes.href.nodeValue.toString();
+    if(href !== null && href.length > 1 && href.substr(0, 1) == '#')
+    {
+        links[i].onclick = function()
+        {
+            var element;
+            var href = this.attributes.href.nodeValue.toString();
+            if(element = document.getElementById(href.substr(1)))
+            {
+                var hop_count = speed/moving_frequency
+                var getScrollTopDocumentAtBegin = getScrollTopDocument();
+                var gap = (getScrollTopElement(element) - getScrollTopDocumentAtBegin) / hop_count;
+
+                for(var i = 1; i <= hop_count; i++)
+                {
+                    (function()
+                    {
+                        var hop_top_position = gap*i;
+                        setTimeout(function(){  window.scrollTo(0, hop_top_position + getScrollTopDocumentAtBegin); }, moving_frequency*i);
+                    })();
+                }
+            }
+
+            return false;
+        };
+    }
+}
+
+var getScrollTopElement =  function (e)
+{
+    var top = 0;
+
+    while (e.offsetParent != undefined && e.offsetParent != null)
+    {
+        top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
+        e = e.offsetParent;
+    }
+
+    return top;
+};
+
+var getScrollTopDocument = function()
+{
+    return document.documentElement.scrollTop + document.body.scrollTop;
+};
+})();
